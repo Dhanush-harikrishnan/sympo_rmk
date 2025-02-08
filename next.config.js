a5f337b4716d30c55bbc13060597dfe4ee26408a
Sheegan Sri G M <sheegangm@gmail.com>
2025-02-08 08:10:03 +0530

    Updated

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Enables static export
-  images: {
-    unoptimized: true, // Needed only if using next/image
-  },
-};
-
-module.exports = nextConfig;
